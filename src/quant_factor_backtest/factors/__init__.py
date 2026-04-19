84416d90168daa533a0d8d99fcdad015f1664ad8
zhuoqun.niu <zhoqun.niu>
2026-04-19 17:52:32 +0800

    重新初始化

--- src/quant_factor_backtest/factors/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base import Factor
-from .builtin import DailyBasicFieldFactor, MomentumFactor, StaticDataFactor
-
-__all__ = ["DailyBasicFieldFactor", "Factor", "MomentumFactor", "StaticDataFactor"]
